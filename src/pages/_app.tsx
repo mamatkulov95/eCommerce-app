807f4514d216e04a1864443d72b96c621007ad91
Ma'rufjon <m.mamatkulov95@gmail.com>
2023-05-23 23:57:36 +0500

    [Header] -Refactoring language switcher

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
-import "../styles/globals.css";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
+import "../styles/globals.css";
+// import { Dropdown, Ripple, initTE } from "tw-elements";
+// initTE({ Dropdown, Ripple });
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const use = async () => {
+      (await import("tw-elements")).default;
+    };
+    use();
+  }, []);
   return (
     <Layout>
       <Component {...pageProps} />
