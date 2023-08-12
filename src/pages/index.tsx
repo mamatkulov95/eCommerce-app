9846b937c8e3f5e42b64deee80d7d93ada1143c6
Ma'rufjon <m.mamatkulov95@gmail.com>
2023-08-12 14:01:05 +0500

    [En-Yenilar] -developed en-yenilar section of the app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import NewProducts from "@/components/newProducts";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <div className="app">
       <Hero />
+      <NewProducts />
     </div>
   );
 }
